34db54c61078453ce15251e837fd74b2eec8313a
Saif Alqady <saifalqady@192.168.1.7>
2023-09-18 16:33:31 +0300

    connect monogdb using client

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express,{Request,Response} from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
 import mongoose from "mongoose";
 import fs from 'fs'
 
@@ -25,12 +26,35 @@ app.use(express.json());
 
 // create a list of all files in images folder
 // const files = fs.readdirSync('images')
+const uri = "mongodb+srv://SaifAlqady:<password>@cluster0.u9fesc1.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+})
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
 // connecting mongodb atlas
-const MONGO_URL = `${process.env.DATABASE_URL}` as string
-mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL).then(() => console.log("connected successfully"));
-mongoose.connection.on("error", (error: Error) => console.log(error));
+// const MONGO_URL = `${process.env.DATABASE_URL}` as string
+// mongoose.Promise = Promise;
+// mongoose.connect(MONGO_URL).then(() => console.log("connected successfully"));
+// mongoose.connection.on("error", (error: Error) => console.log(error));
 
 
 app.use("/api", api);
